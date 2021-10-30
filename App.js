c414de64546c17c737df6306841322c852e29058
Pragyansh Chaturvedi (r41k0u) <pragyanshchaturvedi18@gmail.com>
2021-10-30 12:52:27 +0530

    First Static Page

--- App.js
+++ App.js
@@ -6,107 +6,82 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+ import React from 'react';
+ import type {Node} from 'react';
+ import {
+   Text,
+   View,
+   Image,
+   TextInput,
+   Button,
+   Alert,
+   TouchableOpacity,
+ } from 'react-native';
+ 
+ 
+ 
+ const App = () => {
+   return (
+     <View style={[{flex: 1}, {
+       // Try setting `flexDirection` to `"row"`.
+       flexDirection: "column"
+     }, {backgroundColor: 'white', position: "relative",}]}>
+     <View
+       style={{
+         flex: 64,
+         justifyContent: 'center',
+       }}>
+       <Text style={{
+         fontSize: 22,
+         paddingLeft: 18,
+         color: 'black',
+       }}>Enter UIDAI/VID Number</Text>
+     </View>
+     <View style={{
+       flex: 300,
+       alignItems: 'center',
+     }}>
+       <Image source{...require('./Frame.png')} />
+     </View>
+     <View style={{
+       flex: 88,
+       alignItems: 'center',
+       justifyContent: 'center',
+     }}>
+       <TextInput style={{
+         height: 56,
+         width: 320,
+         borderBottomWidth: 1,
+         backgroundColor: '#C1C1C1',
+         opacity: 0.5,
+         placeholder: "12-Digit AADHAR number / 16-Digit VID number"
+       }}
+       placeholder="12-Digit AADHAR / 16-Digit VID"
+       placeholderTextColor="black"
+       keyboardType="numeric">
+ 
+       </TextInput>
+     </View>
+     <View style={{
+       flex: 72,
+       alignItems: 'center',
+       justifyContent: 'center',
+     }}>
+       <View style={{width: 320,}}>
+       <TouchableOpacity style={{
+         width: 320,
+         backgroundColor: "#6750A4",
+         borderRadius: 100,
+         height: 40,
+       }}><View style={{flex: 1, alignItems: "center", justifyContent: "center", }}><Text>NEXT</Text></View></TouchableOpacity>
+       </View>
+     </View>
+     <View style={{
+       flex: 348,
+     }}>
+ 
+     </View>
+     </View>
+   )
+ }
+ export default App;
---
+++ readme.md
@@ -0,0 +1,3 @@
+abh static page banaya hai.
+materialUI is giving errors for some reason to reac tnative mein likha hai
+sorry for bad cod :'(
