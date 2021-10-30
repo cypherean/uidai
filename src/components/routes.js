413459c00fabf6bb71b70294084489cecf53b551
cypherean <shreyasharma.ss305@gmail.com>
2021-10-30 23:57:23 +0530

    Refactor

--- src/components/routes.js
+++ src/components/routes.js
@@ -1,16 +1,18 @@
-import React from 'react'
-import { Router, Scene } from 'react-native-router-flux'
-import Home from './aadhar.js'
-import QRcode from './qrcode.js'
-import About from './verificationCode.js'
+import React from 'react';
+import {Router, Scene} from 'react-native-router-flux';
+import Home from './aadhar.js';
+import QRcode from './qrcode.js';
+import SplashScreen from './splash_screen.js';
+import About from './verification_code.js';
 
 const Routes = () => (
-   <Router>
-      <Scene key = "root">
-         <Scene key = "home" component = {Home} title = "Verification" initial = {true} />
-         <Scene key = "about" component = {About} title = "Verification" />
-         <Scene key = "qrcode" component = {QRcode} title = "QR Code" />
-      </Scene>
-   </Router>
-)
-export default Routes
+  <Router>
+    <Scene key="root">
+      <Scene key="splashscreen" component={SplashScreen} />
+      <Scene key="home" component={Home} title="Verification" initial={true} />
+      <Scene key="about" component={About} title="Verification" />
+      <Scene key="qrcode" component={QRcode} title="QR Code" />
+    </Scene>
+  </Router>
+);
+export default Routes;
